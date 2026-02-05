cb5ab27a48a3138900143b2938f29740a3223b3d
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-02-05 15:11:43 -0500

    Added reference for event creators

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_05_184232) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_05_200205) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.integer "creator_id"
     t.date "event_date"
     t.string "event_title"
     t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_05_184232) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
