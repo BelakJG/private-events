f1b3ce234a60668fccab444872e7992257b2eea4
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-02-09 13:05:28 -0500

    Scoped past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_many :enrollments
   has_many :attendees, through: :enrollments, source: :user
+
+  scope :past, -> { where("event_date < ?", Date.current) }
+
+  scope :upcoming, -> { where("event_date >= ?", Date.current) }
 end
